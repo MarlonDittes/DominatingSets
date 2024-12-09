f000f49497c73eebee08b820e7d22b9d29fe5b09
Marlon Dittes <mali.dittes@gmail.com>
2024-12-09 02:49:27 +0100

    ADD: automatically run all solvers on instance set via run.sh

--- run.sh
+++ run.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 
-solver="scip"
+solvers=("findminhs" "highs" "scip")
+testset="testset"
+time_limit=120
 
-input_dir="./graphs/testset"
-output_csv="./graphs/testset/$solver.csv"
+input_dir="./graphs/$testset"
 
-echo "Name,Time Taken (seconds)" > "$output_csv"
-
-for graph_file in "$input_dir"/*.gr; do
-    case "$solver" in 
-    "findminhs")
-        time_taken=$( { time ./build/main "$graph_file" findminhs solution.json settings.json; } 2>&1 | grep real | awk '{print $2}' | sed 's/[^0-9.]//g')
-        ;;
-    "highs")
-        time_taken=$( { time ./build/main "$graph_file" highs; } 2>&1 | grep real | awk '{print $2}' | sed 's/[^0-9.]//g')
-        ;;
-    "scip")
-        time_taken=$( { time ./build/main "$graph_file" scip; } 2>&1 | grep real | awk '{print $2}' | sed 's/[^0-9.]//g')
-        ;;
-    *)
-        time_taken="ERROR"
-        ;;
+for solver in "${solvers[@]}"; do
+    output_csv="./graphs/$testset/$solver.csv"
+    
+    # Initialize CSV header based on the solver
+    case "$solver" in
+        "findminhs")
+            echo "Name,Time Taken (seconds)" > "$output_csv"
+            ;;
+        "highs" | "scip")
+            echo "Name,Solution Size,Time Taken (seconds)" > "$output_csv"
+            ;;
+        *)
+            echo "ERROR: Unsupported solver $solver"
+            continue
+            ;;
     esac
+    
+    # Process each graph file for the current solver
+    for graph_file in "$input_dir"/*.gr; do
+        case "$solver" in
+            "findminhs")
+                time_taken=$( { time timeout $time_limit ./build/main "$graph_file" findminhs solution.json settings.json; } 2>&1 | grep real | awk '{split($2, a, /m|s/); print a[1]*60 + a[2]}')
 
-    echo "$(basename "$graph_file"),$time_taken" >> "$output_csv"
+                if [ "$(echo "$time_taken > $time_limit" | bc -l)" -eq 1 ]; then
+                    time_taken=""
+                fi
+
+                echo "$(basename "$graph_file"),$time_taken" >> "$output_csv"
+                ;;
+            "highs" | "scip")
+                ./runlim --time-limit=$time_limit ./build/main "$graph_file" "$solver" > temp.txt
+                output=$(cat temp.txt)
+
+                echo "$(basename "$graph_file"),$output" >> "$output_csv"
+                rm -f temp.txt
+                ;;
+            *)
+                echo "ERROR: Unsupported solver $solver"
+                continue
+                ;;
+        esac
+    done
+
+    echo "Processing complete for $solver. Results saved to $output_csv"
 done
-
-echo "Processing complete. Results saved to $output_csv"
